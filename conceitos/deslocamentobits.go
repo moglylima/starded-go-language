9be2fec6fd8e531ca963a07786c42c4691361508
Your Name <you@example.com>
2022-07-04 09:07:32 -0300

    doc(conceitos/): iota, deslocamento de bits
    Realizado a conclusão do módulo 2, realizar exercícios para concluir nível 2.

---
+++ conceitos/deslocamentobits.go
@@ -0,0 +1,36 @@
+package main
+
+const {
+	   = iota
+	KB = 1 << (iota * 10) // 1 << (1 * 10)
+	MB
+	GB
+	TB
+}
+
+func main(){
+
+	fmt.Println("Basico deslocamento Binário")
+	y := 24
+	x := y >> 2
+	z := y >> 2
+
+	fmt.Printf("%b\t", x)
+	fmt.Printf("%b\t", y)
+	fmt.Printf("%b\t", z)
+
+
+
+
+
+	fmt.Println("Casos de USO")
+	fmt.Println("binary\t\t\t\tdecimal")
+	fmt.Printf("%b\t\t\t", KB)
+	fmt.Printf("%d\n", KB)
+	fmt.Printf("%b\t\t", MB)
+	fmt.Printf("%d\n", MB)
+	fmt.Printf("%b\t", GB)
+	fmt.Printf("%d\n", GB)
+
+	
+}
---
+++ conceitos/iota.go
@@ -0,0 +1,19 @@
+package main
+
+const {
+	//Com iotas é possivel implementar uma logica
+	//onde cara variável sera atibuido um valor sequencial
+	//no exemplo temos 0, 1, 2, 3 porém podemos até mesmo
+	//fazer uso de lógica, como operadores de incremento
+	//somar o valor de iota a um valor x, dentre outrar operações
+	a = iota
+	b =	iota
+	_ =	iota
+	d =	iota
+	e =	iota
+}
+
+func main(){
+	fmt.Println(a, b, d, e)
+
+}
